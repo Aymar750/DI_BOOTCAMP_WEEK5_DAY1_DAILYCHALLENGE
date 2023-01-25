df3d0dfd32a87d1a634a8928931d88c52d92bb75
Aymar <yannyao4066@gmail.com>
2023-01-25 00:54:11 +0000

    daily

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DailyChallenge',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
